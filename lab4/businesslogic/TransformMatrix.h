be70a7a3dc51c2a8a8c4a757754741b6680a9f89
k3rnel1x <k3rnel1x@MacBook-Air-k3rnel1x.local>
2026-05-25 16:49:56 +0300

    impl CreateRotationMatrix

--- lab4/businesslogic/TransformMatrix.h
+++ lab4/businesslogic/TransformMatrix.h
@@ -14,5 +14,4 @@ public:
 
 private:
     double _matrix[MATRIXSIZE][MATRIXSIZE] = {0};
-
 };
--- lab4/businesslogic/TransformMatrixBuilder.cpp
+++ lab4/businesslogic/TransformMatrixBuilder.cpp
@@ -4,11 +4,27 @@
 
 #include "TransformMatrixBuilder.h"
 
-#include <stdexcept>
-
-
+#include <valarray>
 
 TransformMatrix TransformMatrixBuilder::CreateRotationMatrix(double alpha, double beta, double gamma)
 {
-    TransformMatrix matrix;
+    TransformMatrix xRotMatrix {
+        {1, 0, 0},
+        {0, cos(alpha), -sin(alpha)},
+        {0, sin(alpha), cos(alpha)}
+    };
+
+    TransformMatrix yRotMatrix {
+        {cos(beta), 0, sin(beta)},
+        {0, 1, 0},
+        {-sin(beta), 0, cos(beta)},
+    };
+
+    TransformMatrix zRotMatrix{
+        {cos(gamma), -sin(gamma), 0},
+        {sin(gamma), cos(gamma), 0},
+        {0, 0, 1},
+    };
+
+    return xRotMatrix * yRotMatrix * zRotMatrix;
 }
