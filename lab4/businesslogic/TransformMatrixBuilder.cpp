bba388d4cdf2e0d99c401c18166672fb56e34bd2
k3rnel1x <k3rnel1x@MacBook-Air-k3rnel1x.local>
2026-05-25 16:59:13 +0300

    impl CreateScaleMatrix and switch to 4D matrix

--- lab4/businesslogic/TransformMatrixBuilder.cpp
+++ lab4/businesslogic/TransformMatrixBuilder.cpp
@@ -8,21 +8,24 @@
 TransformMatrix TransformMatrixBuilder::CreateRotationMatrix(double alpha, double beta, double gamma)
 {
     TransformMatrix xRotMatrix {
-        {1, 0, 0},
-        {0, cos(alpha), -sin(alpha)},
-        {0, sin(alpha), cos(alpha)}
+        {1, 0, 0, 0},
+        {0, cos(alpha), -sin(alpha), 0},
+        {0, sin(alpha), cos(alpha), 0},
+        {0, 0, 0, 1},
     };
 
     TransformMatrix yRotMatrix {
-        {cos(beta), 0, sin(beta)},
-        {0, 1, 0},
-        {-sin(beta), 0, cos(beta)},
+        {cos(beta), 0, sin(beta), 0},
+        {0, 1, 0, 0},
+        {-sin(beta), 0, cos(beta), 0},
+        {0, 0, 0, 1},
     };
 
     TransformMatrix zRotMatrix{
-        {cos(gamma), -sin(gamma), 0},
-        {sin(gamma), cos(gamma), 0},
-        {0, 0, 1},
+        {cos(gamma), -sin(gamma), 0, 0},
+        {sin(gamma), cos(gamma), 0, 0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 1},
     };
 
     return xRotMatrix * yRotMatrix * zRotMatrix;
@@ -35,5 +38,15 @@ TransformMatrix TransformMatrixBuilder::CreateMoveMatrix(double x, double y, dou
             {0, 1, 0, y},
             {0, 0, 1, z},
             {0, 0, 0, 1},
-        };;
+        };
+}
+
+TransformMatrix TransformMatrixBuilder::CreateScaleMatrix(double a, double b, double c)
+{
+    return TransformMatrix {
+                {a, 0, 0, 0},
+                {0, b, 0, 0},
+                {0, 0, c, 0},
+                {0, 0, 0, 1},
+            };
 }
